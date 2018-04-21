e73bda9fdcd1bb8b083e3da714a32a40eab46112
pacomedomagni <38393799+pacomedomagni@users.noreply.github.com>
2018-04-20 22:43:19 -0400

    Update BaseDAO.java

--- src/main/java/com/gcit/lms/dao/BaseDAO.java
+++ src/main/java/com/gcit/lms/dao/BaseDAO.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public abstract class BaseDAO<T>{
-	
 	@Autowired
 	public JdbcTemplate jdbcTemplate;
 	
